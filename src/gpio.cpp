70f8de9e521625162baccda571407336a6a07cc5
Sidik Nurcahyo <93082861+SidikN@users.noreply.github.com>
2021-10-24 20:15:14 +0700

    Add files via upload

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -1,1 +1,33 @@
+#include "gpio.h"
 
+Gpio::Gpio(int pin, int mode){
+    pinMode(pin,mode);
+    _pin = pin;
+}
+
+Gpio::~Gpio(){
+    
+}
+
+void Gpio::on(){
+    digitalWrite(_pin,HIGH);
+}
+
+void Gpio::off(){
+    digitalWrite(_pin,LOW);
+}
+
+void Gpio::toggle(){
+    if(isOn())
+        off();
+    else
+        on();
+}
+
+bool Gpio::isOn(){
+    return digitalRead(_pin);
+}
+
+bool Gpio::isOff(){
+    return !digitalRead(_pin);
+}
